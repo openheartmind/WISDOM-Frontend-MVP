e3cc7ce7ff0d861ffbe540f4e5c89fb87f465e07
Aryan Tiwari <aryantiwari7762@gmail.com>
2025-04-15 15:35:21 +0530

    refactor: enhance NavBar component by adding back button functionality and cleaning up imports

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,8 +1,7 @@
 "use client";
-
-import { CogIcon, User } from "lucide-react";
+import { ArrowLeft, CogIcon, User } from "lucide-react";
 import * as React from "react";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 enum Roles {
   Admin = 'admin',
@@ -11,7 +10,8 @@ enum Roles {
 
 const NavBar = () => {
   const pathname = usePathname();
-  
+  const router = useRouter();
+
   // Determine title and role based on pathname
   const getTitleAndRole = (path: string) => {
     if (path.includes("/reports")) {
@@ -28,42 +28,52 @@ const NavBar = () => {
     else if (path.includes("/regstration")) {
       return { title: "New User", role: Roles.Creator };
     }
-
     // Default or other paths
     return { title: "Meta-Science", role: undefined };
   };
-  
+
   const { title, role } = getTitleAndRole(pathname);
-  
+
+  const handleGoBack = () => {
+    router.back();
+  };
+
   return (
     <div>
       <div className="flex flex-col h-95 items-center justify-top absolute top-5 left-0 right-0">
         <div className="flex flex-row h-95 items-center w-full max-w-md">
+          {/* Back Button */}
+          <div className="w-10 flex items-center justify-center ml-2">
+            <ArrowLeft 
+              className="h-6 w-6 cursor-pointer" 
+              onClick={handleGoBack}
+            />
+          </div>
+          
           {/* Logo */}
-          <div className="w-32 ml-5">
+          <div className="w-32 ml-3">
             <div className="w-full rounded-full size-20 bg-gray-100 flex items-center justify-center">
               <span className="text-3xl">Logo</span>
             </div>
           </div>
-
+          
           {/* Title */}
           <div className="flex flex-left ml-10 justify-center">
             <label htmlFor="title" className="block text-3xl mb-2 pr-7">
               {title}
             </label>
           </div>
-
+          
           {/* Options */}
           <div className="mx-auto">
-              { role && (role === Roles.Admin ? <CogIcon className="h-10 w-10"  /> : <User className="h-10 w-10"  /> ) }
+            {role && (role === Roles.Admin ? <CogIcon className="h-10 w-10" /> : <User className="h-10 w-10" />)}
           </div>
         </div>
       </div>
       {/* <div className="md:mb-20 mb-15">
-      </div> */}
+       </div> */}
     </div>
-  )
-  
+  );
 };
 
 export { NavBar };
