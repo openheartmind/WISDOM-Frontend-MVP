0e4ce11a1fbe41d348fca81f1d21280fcecdba13
Shay Lavi <shaylavister@gmail.com>
2025-04-21 00:19:04 +1000

    bug fixes for the route

--- src/app/api/terms/route.ts
+++ src/app/api/terms/route.ts
@@ -1,14 +1,14 @@
 export async function GET(req: Request) {
-  const url = new URL(process.env.NEXT_PUBLIC_BASEURL || '', process.env.NEXT_PUBLIC_TERMS_FILENAME).href;
+  const url = new URL(process.env.NEXT_PUBLIC_TERMS_FILENAME as string, process.env.NEXT_PUBLIC_BASEURL).href;
   const data = await fetch(url).then(async (res) => {
     try {
       return await res?.text();
     } catch (err) {
-      return `Server Error: ${err}`;
+      throw Error(`Server Error: ${err}`);
     }
   });
-  return new Response(data, {
-    status: data.toLowerCase().includes("error") ? 500 : 200,
+  return new Response(JSON.stringify(data), {
+    status: JSON.stringify(data).toLowerCase().includes("error") ? 500 : 200,
     headers: { "Content-Type": "application/json" },
   });
 }
