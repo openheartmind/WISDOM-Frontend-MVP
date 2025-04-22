ce3cd1ffa8488dca0a8c93d3a5debf8ecd0bd2b1
rahmsc <danmilad.code@gmail.com>
2025-04-22 07:49:47 +0700

    Added custom dialog component to contribution card

--- src/components/contribution/contribution-card.tsx
+++ src/components/contribution/contribution-card.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Dialog } from "@/components/ui/dialog";
+import { CustomDialog } from "@/components/ui/customdialog";
 import { Info } from "lucide-react";
 
 // Contribution Card Component
@@ -21,32 +21,29 @@ export const ContributionCard = ({
   onVote,
 }: ContributionCardProps) => {
   return (
-    <div className="w-full border rounded-md mb-4 p-4">
+    <div className="w-full border rounded-md mb-4 p-4 relative">
       <div className="flex flex-col items-center text-center">
         <h3 className="text-xl font-medium">{title}</h3>
         <p className="text-lg mb-4">{contributor}</p>
-        <div className="flex space-x-4">
-          <Dialog
-            trigger={
-              <Button
-                variant="ghost"
-                size="icon"
-                className="ml-auto rounded-full"
-                aria-label={`View details about ${title}`}
-              >
-                <Info className="h-6 w-6" aria-hidden="true" />
-              </Button>
-            }
-            title={title}
-            description={contributor}
-            content={description}
-          />
+
+        <div className="w-full flex justify-center mb-2">
           <Button
-            className="flex-1 bg-[#2196F3] hover:bg-[#2196F3]/90 text-white"
+            className="bg-[#2196F3] hover:bg-[#2196F3]/90 text-white px-8"
             onClick={() => onVote(id)}
           >
             Vote
           </Button>
+        </div>
+
+        <div className="absolute top-4 right-4">
+          <CustomDialog title={title} content={description}>
+            <div
+              className="inline-flex items-center justify-center rounded-full h-9 w-9 hover:bg-accent hover:text-accent-foreground cursor-pointer"
+              aria-label={`View details about ${title}`}
+            >
+              <Info className="h-6 w-6" aria-hidden="true" />
+            </div>
+          </CustomDialog>
         </div>
       </div>
     </div>
