c0b13d30af4b1ea690bbcbbecbbe394d19296c73
Shay Lavi <shaylavister@gmail.com>
2025-04-23 13:13:22 +1000

    bugfix for getting T&Cs and displaying it in dialog. better try-catch for router

--- src/app/registration/page.tsx
+++ src/app/registration/page.tsx
@@ -34,7 +34,7 @@ export default function RegistrationForm() {
     if (isLoading) {
       fetch("/api/terms", {
         headers: {
-          'Content-Type': 'application/json',
+          'Content-Type': 'text/plain',
           'Accept': 'text/plain'
         }
       }).then(async (res) => await res.text()).then((data: string) => {
