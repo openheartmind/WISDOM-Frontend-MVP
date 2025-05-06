9359e2ecc6c6edc6b849dda9dd36821918691e4e
Shay Lavi <shaylavister@gmail.com>
2025-05-07 00:35:26 +1000

    added router data validation

--- src/app/api/registration/route.ts
+++ src/app/api/registration/route.ts
@@ -4,16 +4,20 @@ export async function POST(request: Request) {
   const body = await request.json();
 
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/sign-up`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(body),
-    });
+    if (body.email && body.display && body.password) {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/sign-up`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
 
-    const data = await response.json();
+      const data = await response.json();
 
-    const statusCode = data.status || response.status;
-    return NextResponse.json(data, { status: statusCode });
+      const statusCode = data.status || response.status;
+      return NextResponse.json(data, { status: statusCode });
+    } else {
+      throw Error("Invalid data - Missing required field/s")
+    }
   } catch (error) {
     console.error("Error:", error);
     return NextResponse.json(
