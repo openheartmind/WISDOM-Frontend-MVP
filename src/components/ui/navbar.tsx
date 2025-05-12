58601d7af1cdd66854139c96fd09eda1217a3045
Shay Lavi <shaylavister@gmail.com>
2025-05-12 17:06:51 +1000

    minor code cleanups

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -10,7 +10,6 @@ const APP_TITLE = process.env.NEXT_PUBLIC_APP_TITLE || ''
 const TITLE_CHAR_LIMIT = 18
 const GUID_PATTERN = new RegExp('^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$', 'i')
 
-
 interface Role {
   roleId: string;
   roleTitle: string;
@@ -36,13 +35,6 @@ const NavBar = () => {
   const pathname = usePathname();
   const router = useRouter();
 
-  const spinner = <div role="status">
-    <svg aria-hidden="true" className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
-      <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
-    </svg>
-  </div>
-
   const { isAuthenticated, token } = useAuth();
   const [role, setRole] = useState<Role | null>({ roleId: 'blah', roleTitle: Roles.Admin });
   const [instanceTitle, setInstanceTitle] = useState<string>('');
@@ -60,6 +52,12 @@ const NavBar = () => {
     InstanceReports: '/instances/[id]/reports',
   }
 
+  // Logoff if user isn't authorized
+  useEffect(() => {
+    !isAuthenticated && router.push('/login')
+  }, [])
+
+  // Fetch instance ID if needed
   useEffect(() => {
     if (!apiResponse && pathname.toLowerCase().includes('instances/') && pathname !== screensPath.InstanceCreation) {
       const instanceId = pathname.toLowerCase().replace('instances/', '').split('/').slice(1, 2)
@@ -82,6 +80,7 @@ const NavBar = () => {
     }
   }, [pathname, apiResponse])
 
+  // Set screen title for valid paths
   const setScreenTitle = (path: string): string | null => {
     const validPaths = [
       screensPath.instancesList,
@@ -146,6 +145,13 @@ const NavBar = () => {
   const handleGoBack = () => {
     router.back();
   };
+
+  const spinner = <div role="status">
+    <svg aria-hidden="true" className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+      <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+    </svg>
+  </div>
 
   return (
     <div className="relative z-10">
