5df62137888a3ea9db63fa57b7e4bed9c39f8ef3
Shay Lavi <shaylavister@gmail.com>
2025-06-21 17:54:36 +1000

    fixed parameter names, fixed a bug, adjusted the token name

--- src/app/api/registration/route.ts
+++ src/app/api/registration/route.ts
@@ -4,7 +4,7 @@ export async function POST(request: Request) {
   const body = await request.json();
 
   try {
-    if (body.email && body.display && body.password) {
+    if (body.email && body.displayName && body.password) {
       const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/sign-up`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
